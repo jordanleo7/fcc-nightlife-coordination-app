c336831909f79dab62127a05c009a4f57573c0e8
Jordan Johnson <jordanleo7@hotmail.com>
2018-02-23 08:30:22 -0600

    Added server folder and packages

---
+++ server/index.js
@@ -0,0 +1,46 @@
+// Express
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
+const router = express.Router();
+const passport = require('passport');
+require('dotenv').config();
+
+const app = express();
+const cookieSession = require('cookie-session');
+
+const PORT = process.env.PORT || 5000;
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, '../react/build')));
+
+// Cross Origin Resource Sharing
+app.use(cors());
+app.options('*', cors());
+
+
+
+//
+// MongoDB
+//
+//const Poll = require('./models/Poll');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+mongoose.Promise = global.Promise;
+
+
+//
+// Answer API requests.
+//
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended':'true'}));
+
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.resolve(__dirname, '../react/build', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`Express listening on port ${PORT}`))
